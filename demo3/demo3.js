5924bc9b856d028bc95eac30208d7d2f13fbec51
Sam Faber <samfaber@pal-nat186-147-174.itap.purdue.edu>
2021-09-18 17:15:20 -0400

    Changing class on click demo additions

--- demo3/demo3.js
+++ demo3/demo3.js
@@ -3,10 +3,12 @@ const app = new Vue({
     data: {
         message: "Testing some other v- directives:",
         toggle: true,
+        changingClass: 'divBackground',
     },
     methods: {
         set_toggle: function() {
             this.toggle = !this.toggle;
+            this.changingClass = 'biggerFontsize';
         }
     },
     computed: {
