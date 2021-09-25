ade43709f682b9bbf85ad15b998c80728872b6dc
antreshushin <57733790+antreshushin@users.noreply.github.com>
2021-09-25 00:19:05 -0400

    Demo 1 vue drafted

---
+++ demo1/demo1.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        list: [
+            {id:0, txt:"Heya folks"},
+            {id:1, txt:"This is Software Saturdays intermediate lesson 2."},
+            {id:2, txt:"This array is dynamically resizable."}
+        ],
+    }
+});
