ad7a855ab3b37c82c07e2c72b474dcc2d4f3bc63
SamFaber <ccrunner0524@gmail.com>
2021-10-22 18:58:10 -0400

    Updated data.json with correct info and removed my database config from the chkpt2.js config template

--- chkpt2/chkpt2.js
+++ chkpt2/chkpt2.js
@@ -1,12 +1,6 @@
 // Load the database first
 const firebaseConfig = {
-    apiKey: "AIzaSyC-XOvh3AjdCFLYgkMUn6GWRT2GppoagQk",
-  authDomain: "initial-test-ssf21-vuejs.firebaseapp.com",
-  databaseURL: "https://initial-test-ssf21-vuejs-default-rtdb.firebaseio.com",
-  projectId: "initial-test-ssf21-vuejs",
-  storageBucket: "initial-test-ssf21-vuejs.appspot.com",
-  messagingSenderId: "972254851969",
-  appId: "1:972254851969:web:2657927ac5824f23086e80"
+    /* Paste here */
 };
 
 // Initialize Firebase
