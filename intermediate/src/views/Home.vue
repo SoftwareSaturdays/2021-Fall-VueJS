d6443bbc616b4e87ffa8e054f3a8b5d628fde31d
Hadi Ahmed <hadiahmed098@gmail.com>
2021-10-29 21:40:30 -0400

    Added starter code for projects

---
+++ intermediate/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <img alt="Software Saturdays logo" src="../assets/ss_logo_white.jpg">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
